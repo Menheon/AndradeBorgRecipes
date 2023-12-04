74c924c9a753a20c947d9dd166b2a5d346e2c0f3
Kasper Dueholm Borg <kdb@mjolner.dk>
2023-12-04 18:03:29 +0100

    Updated app bar design

--- src/shared/AppBar.tsx
+++ src/shared/AppBar.tsx
@@ -2,11 +2,17 @@ import Logo from "../assets/logo.svg?react";
 
 export const AppBar = () => {
   return (
-    <div className="container flex items-center gap-5 mx-auto py-3 px-5 bg-gray-300">
-      <Logo color="#fff" fill="green" />
-      <a href="/recipes">Recipes</a>
-      <a href="/inspiration">Inspiration</a>
-      <a href="/empty-my-fridge">Empty My Fridge</a>
+    <div className="w-screen flex items-center gap-5 py-3 px-5 bg-lime-700">
+      <Logo />
+      <a href="/recipes" className="text-white font-semibold">
+        Recipes
+      </a>
+      <a href="/inspiration" className="text-white font-semibold">
+        Inspiration
+      </a>
+      <a href="/empty-my-fridge" className="text-white font-semibold">
+        Empty My Fridge
+      </a>
     </div>
   );
 };
