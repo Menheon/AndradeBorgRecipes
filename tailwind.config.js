89c40ad340a2b37b959a73bdcdd73b47ab6cab27
Kasper Dueholm Borg <kdb@mjolner.dk>
2023-12-04 21:30:48 +0100

    Added custom colors to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
+/** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        darkerGreen: "#5a6b07",
+        darkGreen: "#98ae2e",
+        pastelGreen: "#f4f6eb",
+      },
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
+};
