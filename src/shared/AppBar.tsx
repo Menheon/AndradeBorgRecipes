3d5c4f61cc2ebf0e498042c436ac205c399e5662
Kasper Dueholm Borg <kdb@mjolner.dk>
2023-12-04 21:32:31 +0100

    Updated design for AppBar component

--- src/shared/AppBar.tsx
+++ src/shared/AppBar.tsx
@@ -2,15 +2,21 @@ import Logo from "../assets/logo.svg?react";
 
 export const AppBar = () => {
   return (
-    <div className="w-screen flex items-center gap-5 py-3 px-5 bg-lime-700">
+    <div className="w-full flex items-center gap-5 py-3 px-5 bg-darkerGreen sticky top-0">
       <Logo />
-      <a href="/recipes" className="text-white font-semibold">
+      <a href="/recipes" className="text-white font-semibold tracking-wider">
         Recipes
       </a>
-      <a href="/inspiration" className="text-white font-semibold">
+      <a
+        href="/inspiration"
+        className="text-white font-semibold tracking-wider"
+      >
         Inspiration
       </a>
-      <a href="/empty-my-fridge" className="text-white font-semibold">
+      <a
+        href="/empty-my-fridge"
+        className="text-white font-semibold tracking-wider"
+      >
         Empty My Fridge
       </a>
     </div>
