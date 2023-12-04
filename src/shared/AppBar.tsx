dfafb7e13f9ef36fbe456157ed8c569866fbc6b2
Kasper Dueholm Borg <kdb@mjolner.dk>
2023-12-04 17:47:49 +0100

    Added logos and app bar

--- src/shared/AppBar.tsx
+++ src/shared/AppBar.tsx
@@ -1,8 +1,12 @@
+import Logo from "../assets/logo.svg?react";
+
 export const AppBar = () => {
   return (
-    <div className="container flex gap-5 mx-auto py-2 px-5 bg-gray-300">
-      <p>Andrade & Borg Recipes</p>
+    <div className="container flex items-center gap-5 mx-auto py-3 px-5 bg-gray-300">
+      <Logo color="#fff" fill="green" />
       <a href="/recipes">Recipes</a>
+      <a href="/inspiration">Inspiration</a>
+      <a href="/empty-my-fridge">Empty My Fridge</a>
     </div>
   );
 };
