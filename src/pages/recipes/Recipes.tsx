2f837c1afd298896671ee05cc3282cdd2b6b536e
Kasper Dueholm Borg <kdb@mjolner.dk>
2023-12-09 14:19:22 +0100

    Added button for creating recipes

--- src/pages/recipes/Recipes.tsx
+++ src/pages/recipes/Recipes.tsx
@@ -1,3 +1,4 @@
+import AddIcon from "assets/add.svg?react";
 import { recipes } from "data/recipeData";
 import { RecipeItem } from "./RecipeItem";
 import { useState } from "react";
@@ -17,10 +18,24 @@ export const Recipes = () => {
     setFilteredRecipes(newFilteredRecipes);
   };
 
+  const createNewRecipe = () => {};
+
   return (
-    <div className="container mx-auto  bg-white p-4 flex-col justify-center">
-      <div className="rounded-md bg-pastelGreen p-4">
-        <div className="flex gap-1 flex-col">
+    <div className="container mx-auto bg-white p-4 justify-center">
+      <div className="flex gap-4">
+        <div className="rounded-md bg-pastelGreen p-4 flex gap-1 flex-col w-36 h-28 justify-start">
+          <label className="text-darkGreen font-semibold text-center">
+            Create Recipe
+          </label>
+          <button
+            className="self-center hover:bg-darkGreen hover:bg-opacity-30 w-10 h-10 flex justify-center items-center rounded-md"
+            onClick={createNewRecipe}
+          >
+            <AddIcon className="fill-black" />
+          </button>
+        </div>
+
+        <div className="rounded-md bg-pastelGreen p-4 flex gap-1 flex-col h-28">
           <label htmlFor="search" className="text-darkGreen font-semibold">
             Search
           </label>
@@ -29,12 +44,13 @@ export const Recipes = () => {
             className="px-2 border-solid border-darkGreen border-2 rounded-md hover:border-darkerGreen focus:outline-darkerGreen"
             onChange={(event) => onSearchInputValueChanged(event.target.value)}
           />
+          <p>
+            {filteredRecipes.length} recipe
+            {filteredRecipes.length === 1 ? "" : "s"} found
+          </p>
         </div>
-        <p>
-          {filteredRecipes.length} recipe
-          {filteredRecipes.length === 1 ? "" : "s"} found
-        </p>
       </div>
+
       <div>
         {filteredRecipes.map((recipe) => (
           <RecipeItem recipe={recipe} key={recipe.title} />
