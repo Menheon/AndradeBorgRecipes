bbe5696b880659bb2213d37f237f3f44e9659777
Kasper Dueholm Borg <kdb@mjolner.dk>
2024-01-27 12:18:14 +0100

    Implemented `shadcn` library

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,38 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
-/** @type {import("tailwindcss").Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ["class"],
+  content: ["./src/**/*.{ts,tsx}"],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       colors: {
         whiteSmoke: "#f2f2f2",
         lightGrey: "#d1ccc8",
         darkGrey: "#a29c98",
+        lightSlateGrey: "#49433e",
         darkSlateGrey: "#36312d",
       },
       fontFamily: {
@@ -18,6 +42,7 @@ module.exports = {
     },
   },
   plugins: [
+    require("tailwindcss-animate"),
     require("tailwindcss/plugin")(({ addVariant }) => {
       addVariant("search-cancel", "&::-webkit-search-cancel-button");
     }),
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 import svgrPlugin from "vite-plugin-svgr";
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     react(),
     viteTsconfigPaths(),
