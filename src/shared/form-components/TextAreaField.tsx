77bb28e5e9fc07b648ec8c1e5885b6fd5fb12a6b
Kasper Dueholm Borg <kdb@mjolner.dk>
2024-01-27 14:10:38 +0100

    Created `TextAreaField` component using `shadcn`

---
+++ src/shared/form-components/TextAreaField.tsx
@@ -0,0 +1,31 @@
+import { Textarea } from "@/components/ui/textarea";
+
+interface Props {
+  id?: string;
+  onChange: (newValue: string) => void;
+  rows: number;
+  columns: number;
+  placeholder: string;
+  value: string;
+}
+
+export const TextAreaField = ({
+  id,
+  onChange,
+  rows,
+  columns,
+  placeholder,
+  value,
+}: Props) => {
+  return (
+    <Textarea
+      value={value}
+      placeholder={placeholder}
+      id={id}
+      rows={rows}
+      cols={columns}
+      className="resize-none outline-none px-2 py-1 bg-whiteSmoke border-2 rounded-md border-darkSlateGrey placeholder-lightSlateGrey"
+      onChange={(event) => onChange(event.target.value)}
+    />
+  );
+};
