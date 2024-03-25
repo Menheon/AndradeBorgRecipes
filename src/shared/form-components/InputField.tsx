e18237aac31bad4ce848acd69266cca6443c2eed
Kasper Dueholm Borg <kdb@mjolner.dk>
2024-03-25 14:20:10 +0100

    Updated implementation of `InputField` component

--- src/shared/form-components/InputField.tsx
+++ src/shared/form-components/InputField.tsx
@@ -1,5 +1,3 @@
-import { Input } from "@/components/ui/input";
-
 interface Props {
   id?: string;
   onChange: (newValue: string) => void;
@@ -8,38 +6,14 @@ interface Props {
   value: string;
 }
 
-export const InputField = ({
-  id,
-  onChange,
-  size,
-  placeholder,
-  value,
-}: Props) => {
-  const getWidth = () => {
-    let width = "";
-    switch (size) {
-      case "small":
-        width = "w-14";
-        break;
-      case "medium":
-        width = "w-36";
-        break;
-      case "large":
-        width = "w-80";
-        break;
-
-      default:
-        break;
-    }
-    return width;
-  };
+export const InputField = ({ id, onChange, placeholder, value }: Props) => {
   return (
-    <Input
+    <input
       value={value}
       placeholder={placeholder}
       id={id}
       onChange={(event) => onChange(event.target.value)}
-      className={`outline-none px-2 py-1 bg-whiteSmoke border-2 rounded-md border-darkSlateGrey placeholder-lightSlateGrey ${getWidth()}`}
+      className="outline-none px-2 py-1 bg-whiteSmoke border-2 rounded-md border-darkSlateGrey placeholder-lightSlateGrey w-full"
     />
   );
 };
