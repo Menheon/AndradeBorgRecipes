72ff46ff7472f2380e34a006b033ee6d8e487b07
Kasper Dueholm Borg <kdb@mjolner.dk>
2024-03-25 12:49:02 +0100

    Implemented `TextButton` component

---
+++ src/shared/form-components/TextButton.tsx
@@ -0,0 +1,31 @@
+import AddIcon from "@/assets/add.svg?react";
+import { ReactNode } from "react";
+
+interface Props {
+  onClicked: () => void;
+  children: ReactNode | string;
+}
+
+export const TextButton = ({ onClicked, children }: Props) => {
+  return (
+    <button
+      className="
+        flex
+        gap-1
+        font-semibold
+        items-center
+        hover:text-lightSlateGrey
+        py-2 px-4
+        rounded-md
+        outline-none
+        m-1
+        focus-visible:ring
+        focus-visible:ring-darkSlateGrey
+      "
+      onClick={onClicked}
+    >
+      <span>{children}</span>
+      <AddIcon className="fill-darkSlateGrey w-7 h-7" />
+    </button>
+  );
+};
