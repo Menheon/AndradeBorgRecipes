fff6fcff979795dc770fff493970c73dea0603d9
Kasper Dueholm Borg <kdb@mjolner.dk>
2024-03-26 08:48:00 +0100

    Added initail dialog for creating new recipes

---
+++ src/pages/recipes/CreateRecipeDialog.tsx
@@ -0,0 +1,176 @@
+import { BaseDialog } from "@/shared/BaseDialog";
+import { TextInputField } from "@/shared/form-components/TextInputField";
+import { TextAreaField } from "@/shared/form-components/TextAreaField";
+import {
+  Controller,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+import { Recipe, Tag } from "@/types/models";
+import { NewRecipeSections } from "./NewRecipeSections";
+import TagInputField from "@/shared/form-components/TagInputField";
+import { getAllRecipeTags } from "@/data/recipesService";
+import { useCallback, useEffect, useState } from "react";
+
+export interface CreateRecipeFormData extends Recipe {}
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const CreateRecipeDialog = ({ isOpen, onClose }: Props) => {
+  const methods = useForm<CreateRecipeFormData>({
+    mode: "all",
+    defaultValues: {
+      name: "",
+      description: "",
+      imageUrl: "",
+      sections: [],
+      tags: [],
+    },
+  });
+  const { watch, control, handleSubmit, setValue } = methods;
+
+  const createNewRecipe: SubmitHandler<CreateRecipeFormData> = (data) => {
+    console.log(data);
+    closeDialog();
+  };
+
+  const closeDialog = () => {
+    // Clear form state before closing.
+    setValue("name", "");
+    setValue("description", "");
+    setValue("imageUrl", "");
+    setValue("sections", []);
+    setValue("tags", []);
+    onClose();
+  };
+
+  const isCreateFormStateInvalid = () => {
+    return (
+      watch("name") === "" ||
+      watch("description") === "" ||
+      watch("sections").length === 0 ||
+      watch("tags").length === 0
+    );
+  };
+
+  const [existingTags, setExistingTags] = useState<Tag[]>([]);
+
+  const getAllTags = useCallback(async () => {
+    // TODO use tanstack query
+    const tags = await getAllRecipeTags();
+    setExistingTags(tags);
+  }, []);
+
+  useEffect(() => {
+    getAllTags();
+  }, [getAllTags]);
+
+  const onNewTagAdded = (newTag: Tag) => {
+    const currentTags = watch("tags");
+    if (
+      currentTags.some(
+        (tag) => tag.id === newTag.id || tag.name === newTag.name,
+      )
+    )
+      return;
+    currentTags.push(newTag);
+    setValue("tags", currentTags);
+  };
+
+  return (
+    <BaseDialog
+      isPrimaryActionDisabled={isCreateFormStateInvalid()}
+      primaryAction={handleSubmit(createNewRecipe)}
+      primaryActionLabel="create"
+      title="Create a new recipe"
+      isOpen={isOpen}
+      onClose={closeDialog}
+    >
+      <div className="grid grid-cols-9 p-2">
+        <div className="col-start-1 col-end-5">
+          <h3 className="text-polyGreen text-md font-semibold pt-2 pb-0.5">
+            Title
+          </h3>
+          <Controller
+            control={control}
+            name="name"
+            render={({ field }) => (
+              <TextInputField
+                value={field.value}
+                onChange={field.onChange}
+                placeholder="Write the name for the recipe"
+              />
+            )}
+          />
+
+          <h3 className="text-md font-semibold pt-2 pb-0.5">Image URL</h3>
+          <Controller
+            control={control}
+            name="imageUrl"
+            render={({ field }) => (
+              <TextInputField
+                value={field.value}
+                onChange={field.onChange}
+                placeholder="Paste the URL for the image of the dish"
+              />
+            )}
+          />
+
+          <h3 className="text-md font-semibold pt-2 pb-0.5">Tags</h3>
+          <Controller
+            control={control}
+            name="tags"
+            render={({ field }) => (
+              <>
+                <TagInputField
+                  // Apply date time key, to ensure clearing input state.
+                  key={new Date().getTime()}
+                  existingTags={existingTags}
+                  onTagAdd={onNewTagAdded}
+                  addedTags={field.value}
+                />
+                <ul>
+                  {field.value.map((tag) => (
+                    <ul key={tag.id} className="text-black">
+                      {tag.name}
+                    </ul>
+                  ))}
+                </ul>
+              </>
+            )}
+          />
+        </div>
+
+        <div className="col-start-6 col-end-10">
+          <h3 className="text-polyGreen text-md font-semibold pt-2 pb-0.5">
+            Description
+          </h3>
+          <Controller
+            control={control}
+            name="description"
+            render={({ field }) => (
+              <TextAreaField
+                value={field.value}
+                onChange={field.onChange}
+                placeholder="Write the description of the recipe"
+                columns={40}
+                rows={8}
+              />
+            )}
+          />
+        </div>
+
+        <div className="col-start-1 col-end-10">
+          <h3 className="text-md font-semibold pt-2 pb-0.5">Sections</h3>
+          <FormProvider {...methods}>
+            <NewRecipeSections />
+          </FormProvider>
+        </div>
+      </div>
+    </BaseDialog>
+  );
+};
