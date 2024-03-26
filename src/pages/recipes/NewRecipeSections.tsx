2eeb72b01c93e49f7ba0a3420a91b663223fa116
Kasper Dueholm Borg <kdb@mjolner.dk>
2024-03-26 08:46:51 +0100

    Implemented initial sections component for recipe dialog

---
+++ src/pages/recipes/NewRecipeSections.tsx
@@ -0,0 +1,87 @@
+import { TextInputField } from "@/shared/form-components/TextInputField";
+import { TextButton } from "@/shared/form-components/TextButton";
+import CloseIcon from "@/assets/close.svg?react";
+import { Section } from "@/types/models";
+import { useFormContext } from "react-hook-form";
+import { CreateRecipeFormData } from "./CreateRecipeDialog";
+import { IngredientsTable } from "@/shared/form-components/IngredientsTable";
+
+export const NewRecipeSections = () => {
+  const methods = useFormContext<CreateRecipeFormData>();
+  const { watch, setValue } = methods;
+  const sections = watch("sections");
+
+  const addSection = () => {
+    setValue("sections", [
+      ...sections,
+      { title: "", ingredients: [], steps: [] },
+    ]);
+  };
+
+  const removeSection = (sectionIndex: number) => {
+    sections.splice(sectionIndex, 1);
+    setValue("sections", sections);
+  };
+
+  const updateSection = (
+    sectionProp: keyof Section,
+    value: string,
+    index: number,
+  ) => {
+    switch (sectionProp) {
+      case "ingredients":
+        // TODO
+        sections[index].ingredients = [];
+        break;
+      case "title":
+        sections[index].title = value;
+        break;
+      case "steps":
+        // TODO
+        sections[index].steps = [];
+        break;
+      default:
+        break;
+    }
+    setValue("sections", sections);
+  };
+
+  return (
+    <div id="recipe-dialog-sections" className="flex flex-col gap-1">
+      {sections.map((section, index) => (
+        <div
+          key={index}
+          className="m-1 p-3 rounded-md bg-gray-200 flex flex-col gap-1"
+        >
+          <div>
+            <div className="flex justify-between">
+              <h4>Section title</h4>
+              <CloseIcon
+                className="h-6 w-6 fill-darkSlateGrey cursor-pointer hover:fill-lightSlateGrey"
+                onClick={() => removeSection(index)}
+              />
+            </div>
+            <TextInputField
+              value={section.title}
+              onChange={(title) => updateSection("title", title, index)}
+              placeholder="Write section title"
+            />
+          </div>
+          <div>
+            <h4>Steps</h4>
+            <TextInputField
+              value={section.title}
+              onChange={(title) => updateSection("title", title, index)}
+              placeholder="Write section title"
+            />
+          </div>
+          <div>
+            <h4>Ingredients</h4>
+            <IngredientsTable section={index} />
+          </div>
+        </div>
+      ))}
+      <TextButton onClicked={addSection}>Add new section</TextButton>
+    </div>
+  );
+};
