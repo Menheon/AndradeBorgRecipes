c81c68c24f948c3bfa4926a051cb0d01e82f4915
Kasper Dueholm Borg <kasper.d.borg@gmail.com>
2024-03-26 14:24:35 +0100

    Updated navbar with some responsiveness

--- src/shared/AppBar.tsx
+++ src/shared/AppBar.tsx
@@ -1,6 +1,9 @@
 import Logo from "@/assets/logo.svg?react";
+import { useMediaQuery } from "@/util/useMediaQuery";
 
 export const AppBar = () => {
+  const isMin900WidthScreen = useMediaQuery(900);
+
   return (
     <div>
       <div className="w-full flex items-center gap-5 py-3 px-5 sticky top-0">
@@ -22,10 +25,17 @@ export const AppBar = () => {
         >
           Empty My Fridge
         </a>
+        {!isMin900WidthScreen && (
+          <div className="bg-lightGrey p-1  mr-0 ml-auto">
+            <Logo className="fill-current text-darkSlateGrey h-7 w-auto" />
+          </div>
+        )}
       </div>
-      <div className="border-darkSlateGrey bg-lightGrey border-4 p-2 absolute left-0 right-0 mx-auto w-56 top-3 z-10">
-        <Logo className="fill-current text-darkSlateGrey" />
-      </div>
+      {isMin900WidthScreen && (
+        <div className="border-darkSlateGrey bg-lightGrey border-4 p-2 absolute left-0 right-0 mx-auto w-56 top-3 z-10">
+          <Logo className="fill-current text-darkSlateGrey" />
+        </div>
+      )}
     </div>
   );
 };
--- src/util/useMediaQuery.tsx
+++ src/util/useMediaQuery.tsx
@@ -3,10 +3,11 @@ import tailwindConfig from "../../tailwind.config";
 import resolveConfig from "tailwindcss/resolveConfig";
 
 const fullConfig = resolveConfig(tailwindConfig);
-type Breakpoints = "2xs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
+type Breakpoints = "2xs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | number;
 
 export const useMediaQuery = (query: Breakpoints) => {
-  const targetWidth = fullConfig.theme.screens[query];
+  const targetWidth =
+    typeof query === "number" ? `${query}px` : fullConfig.theme.screens[query];
   const media = useMemo(
     () => window.matchMedia(`(min-width: ${targetWidth})`),
     [targetWidth],
