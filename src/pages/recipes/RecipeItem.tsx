ac5273846f9c8261b4b38fdfdf319226d15b7fab
Menheon <kasper.d.borg@gmail.com>
2024-04-14 00:19:44 +0200

    Refactor RecipeItem component to be responsive

--- src/pages/recipes/RecipeItem.tsx
+++ src/pages/recipes/RecipeItem.tsx
@@ -2,33 +2,16 @@
 // import { mapUnitToStringFormat } from "util/util";
 import { Recipe } from "@/types/models";
 import { RemovableTag } from "./RemovableTag";
+import { useMediaQuery } from "@/util/useMediaQuery";
 
 interface Props {
   recipe: Recipe;
 }
 
 export const RecipeItem = ({ recipe }: Props) => {
-  return (
-    <div className="mb-6 mx-12 border-darkSlateGrey border-2 grid grid-cols-8">
-      <img
-        className="w-full h-80 min-h-[100px] object-cover col-span-3"
-        src={recipe.imageUrl}
-      />
-      <div className="px-12 py-8 col-span-5">
-        <h2 className="text-5xl font-bold text-center p-4 text-darkGreen font-caveat tracking-wider">
-          {recipe.name}
-        </h2>
-        <hr className="mb-4 border-t-2 border-darkSlateGrey" />
+  const isMinLargeScreen = useMediaQuery("lg");
 
-        <p className="text-lg">{recipe.description}</p>
-        <div className="flex pt-8 gap-1">
-          {recipe.tags.map((tag) => (
-            <RemovableTag key={tag.id} isRemovable={false}>
-              {tag.name}
-            </RemovableTag>
-          ))}
-        </div>
-        {/* {recipe.sections.map((section, i) => (
+  /* {recipe.sections.map((section, i) => (
           <div className="grid grid-cols-2 gap-4" key={i}>
             <div className="bg-pastelGreen rounded-lg px-5 py-2">
               {recipe.sections.length > 1 && (
@@ -66,7 +49,48 @@ export const RecipeItem = ({ recipe }: Props) => {
               </div>
             </div>
           </div>
-        ))} */}
+        ))} */
+
+  return isMinLargeScreen ? (
+    <div className="mb-6 mx-12 border-darkSlateGrey border-2 grid grid-cols-8 h-80">
+      <img
+        className="w-full h-full min-h-[100px] object-cover col-span-3"
+        src={recipe.imageUrl}
+      />
+      <div className="px-12 py-8 col-span-5 flex-col flex">
+        <h2 className="text-5xl font-bold text-center p-4 text-darkGreen font-caveat tracking-wider">
+          {recipe.name}
+        </h2>
+        <hr className="mb-4 border-t-2 border-darkSlateGrey" />
+
+        <p className="text-lg">{recipe.description}</p>
+        <div className="flex gap-1 mb-0 mt-auto">
+          {recipe.tags.map((tag) => (
+            <RemovableTag key={tag.id} isRemovable={false}>
+              {tag.name}
+            </RemovableTag>
+          ))}
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="mb-6 mx-12 border-darkSlateGrey border-2 grid grid-cols-8 ">
+      <h2 className="p-4 col-span-8 text-5xl font-bold text-center text-darkGreen font-caveat tracking-wider">
+        {recipe.name}
+      </h2>
+      <img
+        className="w-full h-full max-h-56 object-cover col-span-8 border-darkSlateGrey border-y-2"
+        src={recipe.imageUrl}
+      />
+      <div className="p-8 col-span-8 flex-col flex">
+        <p className="text-lg">{recipe.description}</p>
+        <div className="flex gap-1 mb-0 mt-auto pt-2">
+          {recipe.tags.map((tag) => (
+            <RemovableTag key={tag.id} isRemovable={false}>
+              {tag.name}
+            </RemovableTag>
+          ))}
+        </div>
       </div>
     </div>
   );
