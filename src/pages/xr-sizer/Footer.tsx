6081a3a5a2cf75ac638642f3e7e4de96a5a9fbb0
Menheon <kasper.d.borg@gmail.com>
2024-04-21 15:02:55 +0200

    Update Footer component in XR Sizer page

--- src/pages/xr-sizer/Footer.tsx
+++ src/pages/xr-sizer/Footer.tsx
@@ -3,7 +3,7 @@ import Countdown from "./Countdown";
 export const Footer = () => {
   return (
     <>
-      <div className="fixed w-full z-20 -mt-44 bg-gradient-to-r from-orange-500 to-orange-600 pt-1">
+      <div className="fixed w-full z-20 bottom-0 bg-gradient-to-r from-orange-500 to-orange-600 pt-1">
         <div className="bg-white w-full p-4 flex flex-col items-center justify-center">
           <h1 className="text-xl font-bold mb-2">Countdown</h1>
           <Countdown />
