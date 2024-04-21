cad5de5591084e14b8a4233a5f7f76017c6a07ae
Menheon <kasper.d.borg@gmail.com>
2024-04-21 22:16:54 +0200

    Updated styles

--- src/pages/xr-sizer/Footer.tsx
+++ src/pages/xr-sizer/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <>
       <div className="fixed w-full z-20 bottom-0 left-0 bg-gradient-to-r from-orange-500 to-orange-600 pt-1 rounded-t-full">
         <div className="bg-white mx-1 p-4 flex flex-col items-center justify-center rounded-t-full">
-          <h1 className="text-xl font-bold mb-2">Countdown</h1>
+          <h1 className="text-xl font-bold mb-2 tracking-wider">Countdown</h1>
           <Countdown />
         </div>
       </div>
--- src/pages/xr-sizer/XrSizer.tsx
+++ src/pages/xr-sizer/XrSizer.tsx
@@ -7,13 +7,13 @@ export const XrSizer = () => {
   return (
     <>
       <div className="fixed w-full z-20 -mt-[60px] text-center bg-gradient-to-r from-orange-500 to-orange-600 h-[60px] flex items-center justify-center shadow-md">
-        <h1 className="text-4xl text-white select-none font-russo-one">
+        <h1 className="text-4xl text-white select-none font-russo-one drop-shadow-md tracking-wide">
           XrSizer
         </h1>
         <div className="absolute right-1">
           <button
             onClick={() => setUser(undefined)}
-            className="text-white hover:text-slate-50 transition-colors font-bold rounded-md px-3 py-1 text-lg"
+            className="text-white hover:text-slate-50 transition-colors font-bold rounded-md px-3 py-1 text-lg drop-shadow-sm"
           >
             Select User
           </button>
@@ -22,19 +22,19 @@ export const XrSizer = () => {
       <div className="bg-slate-50 w-full h-dvh">
         <div className="pt-2 flex flex-col align-center h-full">
           {!user && (
-            <div className="bg-gradient-to-r from-orange-500 to-orange-600 p-1 rounded-lg mx-4 mt-2">
+            <div className="bg-gradient-to-r from-orange-500 to-orange-600 p-1 rounded-lg mx-4 mt-2 drop-shadow-lg">
               <div className="bg-white rounded-md h-full p-4">
                 <p className="text-2xl font-bold text-center">Select User</p>
                 <div className="flex justify-center gap-10 pt-3">
                   <button
                     onClick={() => setUser("Kasper")}
-                    className="bg-white hover:bg-slate-50 transition-colors font-bold  px-4 py-3 rounded-lg ring-2 ring-orange-500 hover:ring-orange-600 text-xl"
+                    className="drop-shadow-sm bg-white hover:bg-slate-50 transition-colors font-bold  px-4 py-3 rounded-lg ring-2 ring-orange-500 hover:ring-orange-600 text-xl"
                   >
                     Kasper
                   </button>
                   <button
                     onClick={() => setUser("Lylian")}
-                    className="bg-white hover:bg-slate-50 transition-colors font-bold  px-4 py-3 rounded-lg ring-2 ring-orange-500 hover:ring-orange-600 text-xl"
+                    className="drop-shadow-sm bg-white hover:bg-slate-50 transition-colors font-bold  px-4 py-3 rounded-lg ring-2 ring-orange-500 hover:ring-orange-600 text-xl"
                   >
                     Lylian
                   </button>
