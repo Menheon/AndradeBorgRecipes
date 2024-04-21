b6cb667d448e743aa37cdc938dbef3974794570d
Menheon <kasper.d.borg@gmail.com>
2024-04-21 15:02:32 +0200

    Refactor XR Sizer page to use Exercises component and update exercise data

--- src/pages/xr-sizer/Exercises.tsx
+++ src/pages/xr-sizer/Exercises.tsx
@@ -1,33 +1,40 @@
-import { exercises } from "./data/exercises";
+import { Workout } from "./types";
 
-export const Exercises = () => {
+interface Props {
+  workout: Workout;
+}
+
+export const Exercises = ({ workout }: Props) => {
   return (
     <>
-      {exercises.map((exercise) => (
+      {workout.exercises.map((userExercise) => (
         <div className="flex items-center justify-center  rounded-lg shadow-md p-1 mx-4 my-2 bg-gradient-to-r from-orange-500 to-orange-600">
           <div className="flex bg-white w-full rounded-md h-36 items-center">
             <img
-              src={exercise.image}
-              alt={exercise.name}
+              src={userExercise.exercise.image}
+              alt={userExercise.exercise.name}
               className="rounded-md h-32 w-32"
             />
             <div className="flex flex-col flex-1 py-1">
-              <h2 className="text-2xl font-bold ">{exercise.name}</h2>
+              <h2 className="text-2xl font-bold ">
+                {userExercise.exercise.name}
+              </h2>
               <div className="flex gap-1">
                 <p className="text-md font-bold italic pl-2">
-                  {exercise.minRepetitions} - {exercise.maxRepetitions} reps of{" "}
-                  {exercise.sets} sets
+                  {userExercise.exercise.minRepetitions} -{" "}
+                  {userExercise.exercise.maxRepetitions} reps of{" "}
+                  {userExercise.exercise.sets} sets
                 </p>
               </div>
               <div className="flex gap-1 items-center w-full pt-2">
                 <p className="text-xl font-bold w-20">Reps</p>
                 <p className="text-2xl font-bold">
-                  {exercise.currentRepetitions}
+                  {userExercise.currentRepetitions}
                 </p>
               </div>
               <div className="flex gap-1">
                 <p className="text-xl font-bold w-20">Weight</p>
-                <p className="text-2xl font-bold">{exercise.weight}</p>
+                <p className="text-2xl font-bold">{userExercise.weight}</p>
               </div>
             </div>
           </div>
