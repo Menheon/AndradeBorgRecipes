7656565d826ee7730acf4a73b229125e10cd67d7
Menheon <kasper.d.borg@gmail.com>
2024-07-10 12:18:17 +0200

    refactor: Update TextButton component to support optional icon and size prop

--- src/shared/form-components/TextButton.tsx
+++ src/shared/form-components/TextButton.tsx
@@ -4,19 +4,27 @@ import { ReactNode } from "react";
 interface Props {
   onClicked: () => void;
   children: ReactNode | string;
+  iconNode?: ReactNode;
+  size?: "sm" | "md";
 }
 
-export const TextButton = ({ onClicked, children }: Props) => {
+export const TextButton = ({
+  onClicked,
+  children,
+  iconNode,
+  size = "md",
+}: Props) => {
   return (
     <button
       type="button"
-      className="
-        m-1
+      className={`
+        m-${size === "sm" ? 0 : 1}
         flex
         items-center
         gap-1
         rounded-md
-        px-4 py-2
+        px-${size === "sm" ? 2 : 4}
+        py-${size === "sm" ? 0.5 : 2}
         font-semibold
         outline-none
         transition
@@ -24,11 +32,11 @@ export const TextButton = ({ onClicked, children }: Props) => {
         focus-visible:ring-2
         focus-visible:ring-inset
         focus-visible:ring-brown-600
-      "
+      `}
       onClick={onClicked}
     >
-      <span>{children}</span>
-      <AddIcon className="h-7 w-7 fill-brown-600" />
+      {children && <span>{children}</span>}
+      {iconNode ?? <AddIcon className="h-7 w-7 fill-brown-600" />}
     </button>
   );
 };
