052b6f7fe269b1f59b237b136a3fd31077f53f4f
Menheon <kasper.d.borg@gmail.com>
2024-07-13 14:59:48 +0200

    refactor: Update IntegerInputField to use internal state for value handling

--- src/shared/form-components/IntegerInputField.tsx
+++ src/shared/form-components/IntegerInputField.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { TextInputField } from "./TextInputField";
 
 interface Props {
@@ -13,14 +14,17 @@ export const IntegerInputField = ({
   placeholder,
   value,
 }: Props) => {
+  const [internalValue, setInternalValue] = useState(value);
+
   const handleOnChange = (value: string) => {
     const newValue = value.replace(/\D+/g, "");
+    setInternalValue(newValue);
     onChange(newValue);
   };
 
   return (
     <TextInputField
-      value={value}
+      value={internalValue}
       placeholder={placeholder}
       id={id}
       onChange={handleOnChange}
